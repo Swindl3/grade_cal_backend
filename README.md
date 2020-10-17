caf6b55e9487a692d4de7cd694c7701c572b8d0f
Swindl3 <swindlemaxxx@gmail.com>
2020-10-18 00:36:16 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# grade_cal_backend
+ grade calculator
---
+++ server.js
@@ -0,0 +1,48 @@
+const app = require("express")(),
+  server = require("http").Server(app);
+const bodyParser = require("body-parser");
+const ip = require("ip");
+const data = require("../../Application/geneus-takehom-test/backend/geneusdna_query");
+
+const port = process.env.PORT || 8888;
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "5mb" }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader("Access-Control-Allow-Headers", "*");
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
+server.listen(port, () => {
+  console.log("Server starting...");
+  console.log(`http://${ip.address()}:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hi CORALINE.");
+});
+
+app.post("/calculate", (req, res) => {
+  const dataList = req.body;
+  console.log(dataList);
+  let average = 0;
+  let sumGrade = 0;
+  let sumUnit = 0;
+  for (let grade of dataList) {
+    sumGrade += parseFloat(grade.grade) * parseFloat(grade.unit);
+    sumUnit += parseFloat(grade.unit);
+  }
+  console.log("sumGrade", sumGrade);
+  console.log("sumUnit", sumUnit);
+  average = (sumGrade / sumUnit).toFixed(2);
+  res.send({ average: average });
+});
